0650af326227178b6a093436b5941da8d0a8ca53
Mischa Untaga <99098079+MischaU8@users.noreply.github.com>
2023-09-22 20:14:44 +0200

    scenarios

--- title.sql
+++ title.sql
@@ -1,2 +1,6 @@
 insert or ignore into titles ( title_id, title_name, bgg ) values ( 'algeria', 'Algeria', 11293 );
-
+insert or ignore into setups ( title_id, player_count, scenario, options ) values
+	( 'algeria', 2, '1954', '{}' ),
+	( 'algeria', 2, '1958', '{}' ),
+	( 'algeria', 2, '1960', '{}' )
+;
